9f48b460abfdbd14222c7652a7f33eecb7024cee
decllxn <163031213+decllxn@users.noreply.github.com>
2024-05-01 18:11:49 +0300

    Update character.sh

--- myscripts.sh/hackerrank_challenges/character.sh
+++ myscripts.sh/hackerrank_challenges/character.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+#Taking user input
+read CHARACTER
+
+if [[ $CHARACTER =~ [yYnY] ]]
+then
+  if [[ $CHARACTER == Y || $CHARACTER == y]]
+  then
+    echo YES
+  else
+    echo NO
+  fi
+fi
