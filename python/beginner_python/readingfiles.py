4be2e099da6fd9a7a5ce4c67d26a33820dab1a48
decllxn <munene.declan1@gmail.com>
2024-05-09 22:11:47 +0300

    more python

--- python/beginner_python/readingfiles.py
+++ python/beginner_python/readingfiles.py
@@ -12,3 +12,23 @@ employee_file.close() #note extension is used on the variable
 # "r" read file
 # "r+" read and write file
 # "w" write file
+
+
+#How to get information on a file
+#you can print out contents print()
+
+#checking if file is readable = readable
+
+print(employee_file.readable()) # returns boolean
+
+print(employee_file.read())
+
+#readline reads the first line then goes on
+print(employee_file.readline()) #reads first line
+print(employee_file.readline()) #reads second line
+
+#readlines() takes content and stores them in array/list
+print(employee_file.readlines())
+#You can use readlines with for loops
+for employee in employee_file.readlines():
+    print(employee) #Prints out each line
