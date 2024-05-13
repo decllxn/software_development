39d912992e98bfad401385cb6bcd4f4d368b9f3e
decllxn <munene.declan1@gmail.com>
2024-05-13 21:19:08 +0300

    "Testing out pycharm for writing pythonic codes"

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+num1 = input("Enter number: ")
+num2 = input("Enter second number: ")
+
+if num1 == 10:
+    print("Great")
+else:
+    print("That\'s great")
+
+#This is a testing code for pycharm
