d5deafed1a3053f642c1aa7048452c3b82161f88
Declan Munene <munene.declan1@gmail.com>
2024-06-04 18:05:40 +0300

    Firebase hot fix

--- website_projects/fb/js/config.js
+++ website_projects/fb/js/config.js
@@ -1,13 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBIngpwxKgrfbxVJpmVUOfUpk6TioOo3t4",
-    authDomain: "learning-from-my-baby.firebaseapp.com",
-    projectId: "learning-from-my-baby",
-    storageBucket: "learning-from-my-baby.appspot.com",
-    messagingSenderId: "715397666972",
-    appId: "1:715397666972:web:d930326e8c97a6d865b559",
-    measurementId: "G-29G58KV3ZV"
-  };
+  apiKey: "AIzaSyBIngpwxKgrfbxVJpmVUOfUpk6TioOo3t4",
+  authDomain: "learning-from-my-baby.firebaseapp.com",
+  projectId: "learning-from-my-baby",
+  storageBucket: "learning-from-my-baby.appspot.com",
+  messagingSenderId: "715397666972",
+  appId: "1:715397666972:web:d930326e8c97a6d865b559",
+  measurementId: "G-29G58KV3ZV"
+};
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
--- website_projects/fb/js/signup.js
+++ website_projects/fb/js/signup.js
@@ -4,18 +4,16 @@ document.getElementById("button").onclick = function () {
     let password = document.getElementById("password").value;
 
     firebase
-        .auth().createUserWithEmailAndPassword(email, password)
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
             // Signed in
             var user = userCredential.user;
             // Optionally, you can update the user's profile with the name
-            return user.updateProfile({
-                displayName: name
-            });
         })
         .then(() => {
             // Redirect to homepage after successful sign up
-            window.location.href = "./homepage.html";
+            window.location.href = "/homepage.html";
         })
         .catch((error) => {
             var errorCode = error.code;
