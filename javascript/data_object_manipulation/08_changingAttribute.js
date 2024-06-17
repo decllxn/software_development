b13ac3b8e6cc096697db56714b88cad5c9d70609
Declan Munene <munene.declan1@gmail.com>
2024-06-17 17:53:07 +0300

    Js DOM

--- javascript/data_object_manipulation/08_changingAttribute.js
+++ javascript/data_object_manipulation/08_changingAttribute.js
@@ -1,3 +1,6 @@
 // You can also change the value of an html attribute
 // Using the syntax below
 //document.getElementById(id).attribute
+
+document.getElementById("myImage").src = "landscape.jpg"
+// This would change the image
